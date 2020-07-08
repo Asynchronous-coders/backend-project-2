b1f4141cdf8a640573dc52c3fdf0e416949b39d7
Juan Osorio <juan.michael.osorio@gmail.com>
2020-07-07 20:47:56 -0500

    travis set up files

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2/promise');
-
 exports.connect = async () => {
     const connection = await mysql.createConnection({
-        host: process.env.host,
-        port: process.env.port,
-        user: process.env.user,
-        password: process.env.password,
-        database: process.env.database
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     });
     return connection;
 }

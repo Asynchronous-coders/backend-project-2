575e42461fab728041fe3f0a88f384a67c43bc31
Samantha <samanthabarrueta@gmail.com>
2020-07-11 15:58:42 -0500

    cocktail class

--- classes/cocktail.js
+++ classes/cocktail.js
@@ -4,8 +4,41 @@ class Cocktail {
     constructor(cocktail_obj){
         this._cocktail = cocktail_obj;
     }
-    status(){
-        return "200 OK";
+
+    get id() {
+        return this._cocktail.id;
+    }
+
+    get cocktail_name() {
+        return this._cocktail.name;
+    }
+
+    set cocktail_name(cocktail_name) {
+        this._cocktail.name = cocktail_name;
+    }
+
+    get ingredients() {
+        return this._cocktail.ingredients;
+    }
+
+    set ingredients(ingredients) {
+        this._cocktail.ingredients = ingredients;
+    }
+
+    get directions() {
+        return this._cocktail.directions;
+    }
+
+    set directions(directions) {
+        this._cocktail.directions = directions;
+    }
+
+    getLiteral() {
+        return this._cocktail;
+    }
+
+    async cocktailByName(name) {
+        this._cocktail = await cocktailModel.cocktailByName(name);
     }
 }
 
